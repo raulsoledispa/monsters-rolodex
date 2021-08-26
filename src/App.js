a9f9587410bcd021849377d3e7d4390e045a5b37
Raul <soledispa.raul@gmail.com>
2021-08-26 15:24:35 -0500

    fix(App.js): Type corrected

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     return(
         <div className="App">
             <h1>Monster Rolodex</h1>
-            <SearchBox placeholder="search monster"
+            <SearchBox placeholder="search monsters"
                        handleChange={e => this.setState({ searchField: e.target.value})} />
             <CardList monster={filteredMonsters} />
         </div>
